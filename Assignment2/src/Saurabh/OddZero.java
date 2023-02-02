95c0c7649f4887fd10a93a1035e02a509d8d7771
Saurabh mishra <Saurabh mishra@Saurabhmishra>
2023-02-02 18:04:24 +0530

    Saurabh

---
+++ Assignment2/src/Saurabh/OddZero.java
@@ -0,0 +1,26 @@
+package Saurabh;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class OddZero {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter size of the array");
+		int i1=sc.nextInt();
+	
+		List<Integer> arr=new ArrayList();
+		arr.add(sc.nextInt());
+		arr.add(sc.nextInt());
+		arr.add(sc.nextInt());
+		arr.add(sc.nextInt());
+		arr.forEach(s1->{if(s1%2!=0) 
+		System.out.println("this is all odd number  "+s1);});
+
+	}
+
+}
